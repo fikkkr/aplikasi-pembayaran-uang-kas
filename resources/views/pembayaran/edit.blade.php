50ff0de57ecf2f1b76d0b9949a1876652ac8ca9f
fikkkr <fikrimarhas11@gmail.com>
2026-06-01 06:13:44 +0700

    ubah ke komen si edit murid

--- resources/views/pembayaran/edit.blade.php
+++ resources/views/pembayaran/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app')
 
 @section('content')
 <div class="container py-4">
@@ -61,4 +61,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection -->
